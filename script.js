8ee28f7455eca3476a8043bb51fae241e5fb0cf8
Parmvir-S <parmvir.shergill@gmail.com>
2021-02-26 23:44:44 -0800

    added scoreboard

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function light() {
     document.getElementById('backgroundColor').setAttribute("class", "light");
 }
 
+let score = 0;
+
 function rock(val) {
     var compChoice = ["Rock", "Paper", "Scissors"];
     var randCompChoice = compChoice[Math.floor(Math.random() * compChoice.length)];
@@ -13,12 +15,33 @@ function rock(val) {
     if (userInput == "Rock" & randCompChoice == "Rock") {
         document.getElementById("result").innerHTML = "It's A Tie!";
         document.getElementById("Computer").innerHTML = "Computer Chose Rock Too";
+        score = score;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     } else if (userInput == "Rock" & randCompChoice == "Scissors") {
         document.getElementById("result").innerHTML = "You Win!";
         document.getElementById("Computer").innerHTML = "Computer Chose Scissors";
+        score = score + 1;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     } else {
         document.getElementById("result").innerHTML = "You Lose!";
         document.getElementById("Computer").innerHTML = "Computer Chose Paper";
+        score = 0;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     }
 
 }
@@ -30,12 +53,33 @@ function paper(val) {
     if (userInput == "Paper" & randCompChoice == "Paper") {
         document.getElementById("result").innerHTML = "It's A Tie!";
         document.getElementById("Computer").innerHTML = "Computer Chose Paper Too";
+        score = score;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     } else if (userInput == "Paper" & randCompChoice == "Rock") {
         document.getElementById("result").innerHTML = "You Win!";
         document.getElementById("Computer").innerHTML = "Computer Chose Rock";
+        score = score + 1;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     } else {
         document.getElementById("result").innerHTML = "You Lose!";
         document.getElementById("Computer").innerHTML = "Computer Chose Scissors";
+        score = 0;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     }
 }
 
@@ -46,12 +90,33 @@ function scissors(val) {
     if (userInput == "Scissors" & randCompChoice == "Scissors") {
         document.getElementById("result").innerHTML = "It's A Tie!";
         document.getElementById("Computer").innerHTML = "Computer Chose Scissors Too";
+        score = score;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     } else if (userInput == "Scissors" & randCompChoice == "Paper") {
         document.getElementById("result").innerHTML = "You Win!";
         document.getElementById("Computer").innerHTML = "Computer Chose Paper";
+        score = score + 1;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     } else {
         document.getElementById("result").innerHTML = "You Lose!";
         document.getElementById("Computer").innerHTML = "Computer Chose Rock";
+        score = 0;
+        document.getElementById("score").innerHTML = score
+        if (score == 3) {
+            score = 0
+            document.getElementById("score").innerHTML = score
+            alert("You Win!!!")
+        }
     }
 }
 
